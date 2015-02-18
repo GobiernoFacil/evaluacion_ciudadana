87e397051d434616110d78c2d0663034c10eb7f7
el coruco <el.coruco@gmail.com>
2015-02-17 22:43:11 -0600

    data structure for the vas model

---
+++ application/models/vas_model.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/** 
+* Vas model
+*
+* @author @gobiernofacil <howdy@gobiernofacil.com>
+*/
+
+class Vas_model extends CI_Model{
+
+  const TABLE = 'vas';
+  function __construct(){
+    parent::__construct();
+  }
+}
