a2ae23d0229b5af8374f0362e3acce0aa552dc54
hugovom <hugo.om@gmail.com>
2015-03-03 19:29:05 -0600

    update style and home

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,79 +1,58 @@
 <!DOCTYPE html>
-<html lang="en">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="es" class="no-js"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Tú Evalúas</title>
-
-	<style type="text/css">
-
-	::selection{ background-color: #E13300; color: white; }
-	::moz-selection{ background-color: #E13300; color: white; }
-	::webkit-selection{ background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body{
-		margin: 0 15px 0 15px;
-	}
-	
-	p.footer{
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-	
-	#container{
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		-webkit-box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+	<meta name="description" content="Tú evalúas es una iniciativa dirigida a los beneficiarios de los programas públicos federales. ">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href='http://fonts.googleapis.com/css?family=Raleway:700,400,300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
-
-<div id="container">
-	
-	<div id="body">
-		<img src="/img/logov0.png" width="100%">
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+<div class="row">
+	<nav class="col-sm-2 col-sm-offset-10">
+		<ul>
+			<li>Acerca</li>
+			<li>Ingresar</li>
+		</ul>
+	</nav>
 </div>
+
+<div class="container">
+	<header class="row">
+		<div class="col-sm-8 col-sm-offset-2">
+			<h1><img src="/img/logov0.png"></h1>
+			<h2 class="intro">Tu opinión sobre los programas públicos federales ayuda a mejorarlos.</h2>
+			<p>¡<strong>Participa</strong>, eres muy importante!</p>
+		</div>
+	</header>
+
+</div>
+
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-5">
+				<p>Forjado artesanalmente por <a href="http://gobiernofacil.com" class="gobiernofacil">Gobierno Fácil</a></p>
+			</div>
+			<div class="col-sm-7">
+				<ul>
+					<li>¿Qué es?</li>					
+					<li>Datos abiertos</li>
+					<li>Preguntas</li>
+					<li>Términos</li>
+					<li>Privacidad</li>
+					<li>Contacto</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
 
 </body>
 </html>
