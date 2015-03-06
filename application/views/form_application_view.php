73650ee78386521ba87a1b74332bf25b355c2a44
hugovom <hugo.om@gmail.com>
2015-03-06 10:59:23 -0600

    update html

--- application/views/form_application_view.php
+++ application/views/form_application_view.php
@@ -1,16 +1,48 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="es" class="no-js"> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
-  <title>Test form</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Custionario | Test</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href='http://fonts.googleapis.com/css?family=Raleway:700,400,300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
-  <h1>Primer formulario</h1>
-  <div id="main">
-    <form id="survey">
-    
-    </form>
+	<div class="container cuestiona">
+		<h1>Primer formulario</h1>
+		<div id="main" class="row">
+			<div class="col-sm-12">
+				<form id="survey">
+		  
+		  		</form>
+			</div>
+		</div>
   </div>
+  
+  <footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-3">
+				<p>Forjado artesanalmente por <a href="http://gobiernofacil.com" class="gobiernofacil">Gobierno Fácil</a></p>
+			</div>
+			<div class="col-sm-9">
+				<ul>
+					<li>¿Qué es?</li>					
+					<li>Datos abiertos</li>
+					<li>Preguntas</li>
+					<li>Términos</li>
+					<li>Privacidad</li>
+					<li>Contacto</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	</footer>
   
   <!-- JS STUFF -->
   <script>
