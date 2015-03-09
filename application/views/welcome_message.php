9ea0ff8126bc30b32c7b0959874725d6bb3e957b
hugovom <hugo.om@gmail.com>
2015-03-09 10:28:16 -0600

    add logo in svg

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -25,7 +25,7 @@
 <div class="container">
 	<header class="row">
 		<div class="col-sm-8 col-sm-offset-2">
-			<h1><img src="/img/logov0.png"></h1>
+			<h1 class="tuevaluas">Tú Evalúas</h1>
 			<h2 class="intro">Tu opinión sobre los programas públicos federales ayuda a mejorarlos.</h2>
 			<p>¡<strong>Participa</strong>, eres muy importante!</p>
 		</div>
