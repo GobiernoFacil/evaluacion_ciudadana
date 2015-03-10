ea99ba1f69bbc5efe7b8d3d3d176b8448b7b1dce
hugovom <hugo.om@gmail.com>
2015-03-10 11:39:13 -0600

    update about page

--- application/views/home/about_view.php
+++ application/views/home/about_view.php
@@ -1,14 +1,41 @@
 <div class="container">
 	<div class="row">
 		<article>
-		<div class="col-sm-10 col-sm-offset-1">
-			<h1>¿Qué es <strong>Tú Evalúas</strong>?</h1>
-			<p><strong>Tú Evalúas</strong> es una plataforma que habilita a los beneficiarios de los programas del 
-				<strong>Gobierno de la República</strong> para dar su opinión respecto de su experiencia e identificar 
-				oportunidades de mejora para los encargados de los mismos. La plataforma permite a los encargados de los 
-				programas cargar cuestionarios respecto de sus programas, y a los beneficiarios expresar su opinión, 
-				a cual se refleja en tiempo real de manera sencilla, atractiva y disponible al público en formato de datos abiertos.</p>
-		</div>
+			<div class="col-sm-10 col-sm-offset-1">
+				<h1>¿Qué es <strong>Tú Evalúas</strong>?</h1>
+				<p><strong>Tú Evalúas</strong> es una plataforma digital que tiene como objetivo recabar la opinión ciudadana sobre los 
+					programas públicos federales.</p>
+				<p>Mediante <strong>Tú Evalúas</strong> podrás valorar los procesos seguidos por cada programa y expresar tu 
+					satisfacción con los productos y servicios que ofrecen.</p>
+				<p><strong>Tú Evalúas</strong> facilitará la colaboración entre el gobierno y la ciudadanía partiendo de una base 
+					tecnológica para:</p>
+				<ol>
+					<li>Conocer la opinión ciudadana sobre la operación de los programas											</li>
+					<li>Mejorar la evaluación de los programas con base en las necesidades expresadas directamente por la ciudadanía</li>
+					<li>Generar espacios de aprendizaje que conduzcan a mejores políticas públicas.									</li>
+				</ol>
+				<p>La meta es que <strong>Tú Evalúas</strong> reúna toda la información referente a la satisfacción de la ciudadanía 
+					con respecto a los programas públicos federales para que quien lo desee (ciudadanos, 
+					asociaciones, gobierno o la comunidad en conjunto) pueda dar seguimiento al pulso ciudadano.
+				
+				<h2>Nosotros</h2>
+				<p>Esta iniciativa nace como parte del proyecto <a href="http://www.presidencia.gob.mx/agentesdeinnovacion/">Agentes de Innovación</a> 
+					de la <a href="http://www.presidencia.gob.mx/edn/">Coordinación de la Estrategia Digital Nacional de 
+					la Presidencia de la República</a>, cuyos objetivos son impulsar el gobierno digital, promover la apertura, la transparencia 
+					y la participación ciudadana mediante el uso de las  tecnologías de la información y la comunicación (TIC). </p>
+				
+				<p>Así, <strong>Tú Evalúas</strong> nace de la cooperación entre la Presidencia de la República a través de la Coordinación de Estrategia 
+					Digital Nacional, la Secretaría de Hacienda y Crédito Público a través de la Unidad de Evaluación del Desempeño, CREA, A.C y 
+					<a href="http://gobiernofacil.com/" title="Gobierno Fácil">Gobierno Fácil</a>. 
+				</p>
+				<p><strong>Tú Evalúas</strong> busca consolidarse como una herramienta para que los ciudadanos participen de la configuración 
+					de los objetivos y 
+					las políticas públicas de las dependencias del gobierno federal. Mediante la aplicación de cuestionarios a beneficiarios y 
+					no beneficiarios de los programas públicos, así como de la publicidad de encuestas de satisfacción, será posible medir 
+					la calidad de los programas desde la perspectiva ciudadana, así como acercar esa perspectiva tanto a los tomadores de decisiones 
+					como al público en general.
+				</p>
+			</div>
 		</article>
 	</div>
 </div>
