0d14e29458f48cc5a6eecf8b065f6adb45a2a513
hugovom <hugo.om@gmail.com>
2015-03-10 20:14:13 -0600

    add faqs

--- application/views/home/preguntas_view.php
+++ application/views/home/preguntas_view.php
@@ -1,13 +1,27 @@
 <div class="container">
 	<div class="row">
 		<article>
-		<div class="col-sm-10 col-sm-offset-1">
+		<div class="col-sm-8 col-sm-offset-2">
 			<h1>Preguntas Frecuentes sobre la plataforma <strong>Tú Evalúas</strong></h1>
-			<p><strong>Tú Evalúas</strong> es una plataforma que habilita a los beneficiarios de los programas del 
-				<strong>Gobierno de la República</strong> para dar su opinión respecto de su experiencia e identificar 
-				oportunidades de mejora para los encargados de los mismos. La plataforma permite a los encargados de los 
-				programas cargar cuestionarios respecto de sus programas, y a los beneficiarios expresar su opinión, 
-				a cual se refleja en tiempo real de manera sencilla, atractiva y disponible al público en formato de datos abiertos.</p>
+			<h2>¿Cómo van a usar mis respuestas?</h2>
+			<p>El portal tiene por objetivo recabar la opinión ciudadana sobre los programas públicos federales. Los resultados de los cuestionarios serán de carácter público en tiempo real y en bases de datos en formato abierto. </p>
+			<p>Los resultados serán un insumo para medir la calidad de los programas desde la perspectiva ciudadana y acercar esa perspectiva tanto a los ejecutores de los programas en las dependencias federales  como al público en general.</p>
+			
+			<h2>¿Cómo fueron obtenidos mis datos de contacto para invitarme a participar en una encuesta?</h2>
+			<p>El portal <strong>Tú Evalúas</strong> es una iniciativa del gobierno federal y la información utilizada fue recabada por cada programa en apego al Título IV de la Ley Federal de Transparencia y Acceso a la Información, referente a la protección de datos personales. </p>
+			
+			<h2>¿Cómo puedo participar en una encuesta?</h2>
+			<p>A través de <strong>Tú Evalúas</strong>, los ejecutores de cada programa envían invitaciones a los beneficiarios de los programas y/o a solicitantes que registraron sus datos a través de los sistemas asociados a cada programa. </p>
+
+			<h2>Al responder los cuestionarios del portal Tú Evalúas ¿existe algún riesgo de perder el apoyo o subsidio con el que cuento?</h2>
+			<p><strong>NO.</strong> Las opiniones recabadas en el sitio <strong>Tuevaluas.com.mx</strong> no pueden asociarse con la persona que las emite, 
+				por lo que de ninguna manera pueden ser utilizadas para condicionar los apoyos o subsidios gubernamentales. </p>
+
+			<h2>¿Qué significa que mis respuestas a los cuestionarios serán utilizadas como datos abiertos?</h2>
+			<p>Que las respuestas a los cuestionarios se concentrarán en un formal xls o csv (bases de datos agregadas) y serán de dominio público en un formato accesible para su uso, reutilización y redistribución para cualquier fin legal que se desee. </p>
+			
+			<h2>¿Cómo se garantiza la veracidad de la información del portal Tú Evalúas?</h2>
+			<p>Los resultados de las encuestas que se realizan en el portal <strong>Tú Evalúas</strong> son cargados en el sistema y publicados en tiempo real, lo que implica que no hay ninguna organización pública o privada que previamente valide o modifique la información que se despliega. </p>
 		</div>
 		</article>
 	</div>
