adebd770da6b36f3b42a9e7ade4d4a8134d8ab74
hugovom <hugo.om@gmail.com>
2015-03-11 12:36:38 -0600

    add open_data view

---
+++ application/views/data/data_view.php
@@ -0,0 +1,15 @@
+<div class="container">
+	<div class="row">
+		<article class="data_hm">
+			<div class="col-sm-8 col-sm-offset-2">
+				<h1>Resultados de cuestionarios en <strong>Tú Evalúas</strong></h1>
+				<section>
+					<h2><?php echo anchor('open_data/index/1','Prospera');?></h2>
+					<?php echo anchor('open_data/index/1','<figure>
+						<img src="/img/programas/prospera.jpg">
+					</figure>');?>
+				</section>
+			</div>			
+		</article>
+	</div>
+</div>
