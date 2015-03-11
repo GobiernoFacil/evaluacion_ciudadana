17b45fad2b42cdee8eaa5077289b2afd5fb87b56
hugovom <hugo.om@gmail.com>
2015-03-11 17:12:02 -0600

    update results

--- application/views/data/singledata_viz_view.php
+++ application/views/data/singledata_viz_view.php
@@ -7,23 +7,32 @@
 					<h2>Participantes: <strong><?php echo $response['applicants'];?></strong></h3>
 
 				</section>
-			
-				<?php foreach($response['questions'] as $question):?>
-					<div>
-						<h3><?php	echo $question->question;?></h3>
-											
-					</div>
-					<?php 
-						echo "<pre>";var_dump($question);
-						echo "</pre>";?>
-	 			<?php endforeach;?>
-				
 				<div>
-					<h5>Preguntas</h5>
-				</div>
-			<?php	echo "<pre>";
-	 	  var_dump($response);
-	 	  echo "</pre>";?>
+					<h2>Respuestas</h2>
+					<ol>
+					<?php foreach($response['questions'] as $question):?>
+						<li>
+							<h3><?php	echo $question->question;?></h3>
+							<?php if(empty($question->options)):?>
+								<?php foreach($question->answers as $respuesta):?>
+									<p>
+									<?php echo $respuesta->num_value;?>
+									</p>
+								<?php endforeach;?>
+							<?php else:?>
+								<ul>
+								<?php foreach($question->options as $respuesta):?>
+									<li>
+									<?php echo '<strong>' . $respuesta->answer_num . '</strong>: ' . $respuesta->description;?>
+									</li>
+								<?php endforeach;?>
+										
+								</ul>
+							<?php endif;?>					
+						</li>
+					<?php endforeach;?>
+					</ol>
+	 			</div>
 			</div>			
 		</article>
 	</div>
