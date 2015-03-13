8358557356aea7d148477958c154413e6ffa281e
hugovom <hugo.om@gmail.com>
2015-03-12 18:54:00 -0600

    update content and style

--- application/views/data/data_view.php
+++ application/views/data/data_view.php
@@ -4,12 +4,12 @@
 			<div class="col-sm-8 col-sm-offset-2">
 				<h1>Resultados de cuestionarios en <strong>Tú Evalúas</strong></h1>
 				<section>
-					<h2><?php echo anchor('datos/1','Prospera');?></h2>
+					<h2><?php echo anchor('datos/1','Jóvenes Prospera');?></h2>
 					<?php echo anchor('datos/1','<figure>
 						<img src="/img/programas/prospera.jpg">
 					</figure>');?>
 					<p class="lead">Opiniones sobre Prospera para mejorar la forma en que el programa atiende a los beneficiarios.
-						<?php echo anchor('datos/1','Resultados', array('class'=>'btn'));?>
+						<?php echo anchor('datos/1','Consulta los resultados', array('class'=>'btn'));?>
 					</p>
 				</section>
 			</div>			
