e2c7f0a8b1c452b928878ef396eaf9031c7620ef
el coruco <el.coruco@gmail.com>
2015-03-30 12:26:24 -0600

    ahí va el admin

---
+++ application/controllers/wackyland/tuevaluas.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Tuevaluas extends CI_Controller {
+
+  function __construct(){
+    parent::__construct();
+  }
+
+  public function index()
+  {
+  }
+}
---
+++ application/views/wackyland/login_view.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Pase usted</title>
+  </head>
+  <body>
+    <form name="nock-nock" method="post">
+      <p><input type="text" name="email"></p>
+      <p><input type="password" name="pass"></p>
+      <p><input type="submit" value="acceder"></p>
+    </form>
+  </body>
+</html>
