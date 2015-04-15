dfc6af0fbcb007f2a359d7ac5947d88b5b286c1f
hugovom <hugo.om@gmail.com>
2015-04-15 18:25:18 -0500

    add style to survey dashboard

--- application/views/wackyland/surveys_view.php
+++ application/views/wackyland/surveys_view.php
@@ -1,26 +1,47 @@
 <div class="container">
 	<div class="row">
-    <h1 class="title">Encuestas</h1>
-
-    <form name="add-survey" method="post" action="<?= site_url("wackyland/surveys/create"); ?>">
-      <h2>Crear encuesta</h2>
-      <p><label>Título</label><input type="text" name="title"></p>
-      <p><input type="submit" value="crear encuesta"></p>
-    </form>
-
-    <section>
-      <h2>Encuestas</h2>
-      <ul>
-      <?php foreach($surveys as $survey): ?>
-        <li>
-          <a href="<?= site_url("wackyland/surveys/update/" . $survey->id); ?>">
-            <?php echo $survey->title; ?>
-          </a>
-
-          <a href="<?= site_url("wackyland/surveys/delete/" . $survey->id); ?>">Eliminar</a>
-        </li>
-      <?php endforeach; ?>
-      </ul>
-    </section>
+		<div class="col-md-12">
+			<h1 class="title">Encuestas</h1>
+			<section class="box">
+			  	<h2>Crear encuesta</h2>
+				<form name="add-survey" method="post" class="row" action="<?= site_url("wackyland/surveys/create"); ?>">
+				  <div class="col-sm-1">
+				  	<label>Título: </label> 
+				  </div>
+				  <div class="col-sm-8">
+			  	 	  <input type="text" name="title">
+				  </div>
+				  <div class="col-sm-3">
+				 	 <p><input type="submit" value="crear encuesta"></p>
+				  </div>
+				</form>
+			</section>
+			
+			<section class="box">
+			  <h2>Encuestas</h2>
+			  <ul class="list">
+				  <li class="row los_titles">
+				  	 <div class="col-sm-10">
+					  	 <h4>Nombre</h4>
+				  	 </div>
+				  	 <div class="col-sm-2">
+					  	  <h4>Acciones</h4>
+				  	 </div>
+				  </li>
+			  <?php foreach($surveys as $survey): ?>
+			    <li class="row">
+			      <div class="col-sm-10">
+			      <a href="<?= site_url("wackyland/surveys/update/" . $survey->id); ?>">
+			        <?php echo $survey->title; ?>
+			      </a>
+			      </div>
+			       <div class="col-sm-2">
+				   		<a href="<?= site_url("wackyland/surveys/delete/" . $survey->id); ?>" class="danger">Eliminar</a>
+			       </div>
+			    </li>
+			  <?php endforeach; ?>
+			  </ul>
+			</section>
+		</div>
 	</div>
 </div>
