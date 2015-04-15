772399140db7eacc3b717ce13eb7db3495627b27
hugovom <hugo.om@gmail.com>
2015-04-15 12:32:29 -0500

    update style of the login

--- application/views/wackyland/login_view.php
+++ application/views/wackyland/login_view.php
@@ -3,12 +3,37 @@
   <head>
     <meta charset="utf-8">
     <title>Pase usted</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="/img/favicon.ico">
+	<link rel="stylesheet" href="/css/normalize.css">
+    <link rel="stylesheet" href="/css/styles.css">
   </head>
-  <body>
-    <form name="nock-nock" method="post">
-      <p><input type="text" name="email"></p>
-      <p><input type="password" name="pass"></p>
-      <p><input type="submit" value="acceder"></p>
-    </form>
+  <body class="login">
+	  <div class="container">
+		  <div class="row">
+		  	<div class="col-sm-8 col-sm-offset-2">
+				<h1 class="tu_evaluas">Tú Evalúas</h1>
+			</div>
+		  	<div class="col-sm-6 col-sm-offset-3">
+		  		<form name="nock-nock" method="post">
+    			  <p><label>Email</label>
+	    			  <input type="text" name="email"></p>
+    			  <p><label>Contraseña</label>
+	    			  <input type="password" name="pass"></p>
+    			  <p><input type="submit" value="acceder"></p>
+    			</form>
+		  	</div>
+		  </div>
+	  </div>
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45473222-7', 'auto');
+  ga('send', 'pageview');
+
+</script>
   </body>
 </html>
