41075391d0bfecf56d7fb2e2752f403222849841
elcoruco <el.coruco@gmail.com>
2016-01-30 23:16:09 -0600

    remove the curious uers :/

--- application/views/wackyland/admins_view.php
+++ application/views/wackyland/admins_view.php
@@ -35,7 +35,10 @@
 					  <p><label>contraseña</label><input id="the-new-pass" type="password" name="password" value=""></p>
 					  <p>Tipo de administrador</p>
 					  <ul class="options">
-					    <li><label><input type="radio" name="level" value="1" checked="checked">curioso</label></li>
+
+					    <!-- THIS KIND OF USER NO LONGER EXIST
+              <li><label><input type="radio" name="level" value="1" checked="checked">curioso</label></li>
+              -->
 					    <li><label><input type="radio" name="level" value="3">funcionario</label></li>
 					    <li><label><input type="radio" name="level" value="5">administrador</label></li>
 					  </ul>
